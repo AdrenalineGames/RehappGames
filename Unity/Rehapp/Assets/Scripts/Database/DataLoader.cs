dfd335d38bbb6c454cad777c9558089a3d829b2c
AdrenalineGames <AdrenalineGames@users.noreply.github.com>
2018-09-08 12:41:17 -0500

    Database testing

---
+++ Unity/Rehapp/Assets/Scripts/Database/DataLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour {
+    public string dbLink = "";
+    public bool connect = false;
+
+    public string[] data;
+
+	// Use this for initialization
+	IEnumerator Connect () {
+        WWW pacientData = new WWW(dbLink);
+        yield return pacientData;
+        string pacientDataString = pacientData.text;
+        //Debug.Log(pacientDataString);
+
+        data = pacientDataString.Split('|');
+	}
+
+    void Update()
+    {
+        if (connect)
+        {
+            StartCoroutine(Connect());
+            connect = false;
+        }
+    }
+}
