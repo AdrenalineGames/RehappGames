cbb13d578e65c4e4d9db5d9ee09c7b478165bec5
AdrenalineGames <AdrenalineGames@users.noreply.github.com>
2018-09-09 10:07:04 -0500

    NewPatient to database

---
+++ Unity/Rehapp/Assets/Scripts/Database/NewPatient.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPatient : MonoBehaviour {
+
+    public string url;
+    public string mahavirId;
+    public string username;
+    public string password;
+
+    public bool add = false;
+
+	public void AddNewPatient(string mID, string un, string pass, string u)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("mahavirIDPost", mID);
+        form.AddField("usernamePost", un);
+        form.AddField("passPost", pass);
+
+        WWW www = new WWW(u, form);
+    }
+
+    private void Update()
+    {
+        if (add)
+        {
+            AddNewPatient(mahavirId, username, password, url);
+            add = false;
+        }
+    }
+}
