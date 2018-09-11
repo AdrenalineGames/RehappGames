c2928fa795171e1524ea7ba4c95140e81f5ab373
AdrenalineGames <AdrenalineGames@users.noreply.github.com>
2018-09-11 12:38:49 -0500

    Login

---
+++ Unity/Rehapp/Assets/Scripts/Database/Login.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour {
+
+    public string loginUrl;
+    public string username;
+    public string password;
+
+    public bool login = false;
+
+	IEnumerator PatientLogin (string un, string pw, string db) {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", un);
+        form.AddField("passPost", pw);
+
+        WWW www = new WWW(db, form);
+        yield return www;
+
+        Debug.Log(www.text);
+    }
+	
+
+	void Update () {
+        if (login)
+        {
+            StartCoroutine(PatientLogin(username, password, loginUrl));
+            login = false;
+        }
+	}
+}
