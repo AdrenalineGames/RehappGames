4df38db5f39a529b22a2d8630e6dcc7c804d6d29
AdrenalineGames <AdrenalineGames@users.noreply.github.com>
2018-10-09 20:23:04 -0500

    Beta 2018.10.09
    Skiing graphic finished

---
+++ Unity/Rehapp/Assets/Scripts/Utilities/MovementEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementEffect : MonoBehaviour
+{
+    public float posRestriction;
+
+    private void Update()
+    {
+        //if(Time.deltaTime != 0)
+        Position();
+    }
+
+    private void Position()
+    {
+        transform.position -= new Vector3(0, 0, PlayerPosition.PlayerFrontalSpeed * Time.deltaTime);
+        if (transform.position.z < posRestriction)
+            Destroy(gameObject);
+    }
+}
---
+++ Unity/Rehapp/Assets/Scripts/Utilities/TileGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour {
+
+    public Transform tileFront;
+    public Transform tileBack;
+    public Transform tile;
+    public Transform parent;
+
+
+	void Update ()
+    {
+        if (tileFront == null)
+        {
+            tileFront = tileBack;
+            Vector3 newTilePos = new Vector3(0, -1.3f, 41);
+            tileBack = Instantiate(tile, newTilePos, Quaternion.Euler(new Vector3(92.15198f, 89.99999f, 89.99999f)), parent);
+        }
+	}
+}
